808ed1108db081077ad6ea5aab7b7ac6bc84e636
is2021 <ingieneriadesoftware2@gmail.com>
2022-03-21 21:55:57 -0300

    añadidas las ventanas

---
+++ views/Ui_editar_libro.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'editar_libro.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class editLibro(object):
+    def setupUi(self, editLibro):
+        if not editLibro.objectName():
+            editLibro.setObjectName(u"editLibro")
+        editLibro.resize(800, 600)
+        self.label = QLabel(editLibro)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 20, 781, 31))
+        self.label.setFrameShape(QFrame.Box)
+        self.label_2 = QLabel(editLibro)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(20, 100, 71, 20))
+        self.lineEdit = QLineEdit(editLibro)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(110, 100, 91, 20))
+        self.label_3 = QLabel(editLibro)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(20, 140, 71, 20))
+        self.lineEdit_2 = QLineEdit(editLibro)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setGeometry(QRect(110, 140, 91, 20))
+        self.label_4 = QLabel(editLibro)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(240, 100, 101, 20))
+        self.lineEdit_3 = QLineEdit(editLibro)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.lineEdit_3.setGeometry(QRect(350, 100, 91, 20))
+        self.label_5 = QLabel(editLibro)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(240, 140, 101, 20))
+        self.buscarPorComboBox_2 = QComboBox(editLibro)
+        self.buscarPorComboBox_2.setObjectName(u"buscarPorComboBox_2")
+        self.buscarPorComboBox_2.setGeometry(QRect(350, 140, 91, 20))
+        self.buscarPorComboBox_3 = QComboBox(editLibro)
+        self.buscarPorComboBox_3.setObjectName(u"buscarPorComboBox_3")
+        self.buscarPorComboBox_3.setGeometry(QRect(580, 100, 91, 20))
+        self.label_6 = QLabel(editLibro)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(470, 100, 101, 20))
+        self.buscarPorComboBox_4 = QComboBox(editLibro)
+        self.buscarPorComboBox_4.setObjectName(u"buscarPorComboBox_4")
+        self.buscarPorComboBox_4.setGeometry(QRect(580, 140, 91, 20))
+        self.label_7 = QLabel(editLibro)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setGeometry(QRect(470, 140, 101, 20))
+        self.label_8 = QLabel(editLibro)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setGeometry(QRect(20, 240, 71, 20))
+        self.lineEdit_4 = QLineEdit(editLibro)
+        self.lineEdit_4.setObjectName(u"lineEdit_4")
+        self.lineEdit_4.setGeometry(QRect(160, 240, 341, 20))
+        self.label_9 = QLabel(editLibro)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setGeometry(QRect(10, 180, 781, 31))
+        self.label_9.setFrameShape(QFrame.Box)
+        self.label_10 = QLabel(editLibro)
+        self.label_10.setObjectName(u"label_10")
+        self.label_10.setGeometry(QRect(20, 270, 111, 20))
+        self.lineEdit_5 = QLineEdit(editLibro)
+        self.lineEdit_5.setObjectName(u"lineEdit_5")
+        self.lineEdit_5.setGeometry(QRect(160, 270, 341, 20))
+        self.lineEdit_6 = QLineEdit(editLibro)
+        self.lineEdit_6.setObjectName(u"lineEdit_6")
+        self.lineEdit_6.setGeometry(QRect(160, 300, 341, 20))
+        self.label_11 = QLabel(editLibro)
+        self.label_11.setObjectName(u"label_11")
+        self.label_11.setGeometry(QRect(20, 300, 111, 20))
+        self.label_12 = QLabel(editLibro)
+        self.label_12.setObjectName(u"label_12")
+        self.label_12.setGeometry(QRect(20, 360, 71, 20))
+        self.lineEdit_8 = QLineEdit(editLibro)
+        self.lineEdit_8.setObjectName(u"lineEdit_8")
+        self.lineEdit_8.setGeometry(QRect(110, 360, 91, 20))
+        self.lineEdit_9 = QLineEdit(editLibro)
+        self.lineEdit_9.setObjectName(u"lineEdit_9")
+        self.lineEdit_9.setGeometry(QRect(110, 400, 91, 20))
+        self.label_15 = QLabel(editLibro)
+        self.label_15.setObjectName(u"label_15")
+        self.label_15.setGeometry(QRect(20, 400, 71, 20))
+        self.lineEdit_10 = QLineEdit(editLibro)
+        self.lineEdit_10.setObjectName(u"lineEdit_10")
+        self.lineEdit_10.setGeometry(QRect(370, 360, 91, 20))
+        self.lineEdit_11 = QLineEdit(editLibro)
+        self.lineEdit_11.setObjectName(u"lineEdit_11")
+        self.lineEdit_11.setGeometry(QRect(370, 400, 91, 20))
+        self.label_16 = QLabel(editLibro)
+        self.label_16.setObjectName(u"label_16")
+        self.label_16.setGeometry(QRect(230, 400, 131, 20))
+        self.label_13 = QLabel(editLibro)
+        self.label_13.setObjectName(u"label_13")
+        self.label_13.setGeometry(QRect(230, 360, 71, 20))
+        self.lineEdit_12 = QLineEdit(editLibro)
+        self.lineEdit_12.setObjectName(u"lineEdit_12")
+        self.lineEdit_12.setGeometry(QRect(620, 360, 91, 20))
+        self.lineEdit_13 = QLineEdit(editLibro)
+        self.lineEdit_13.setObjectName(u"lineEdit_13")
+        self.lineEdit_13.setGeometry(QRect(620, 400, 91, 20))
+        self.label_17 = QLabel(editLibro)
+        self.label_17.setObjectName(u"label_17")
+        self.label_17.setGeometry(QRect(500, 400, 111, 20))
+        self.label_14 = QLabel(editLibro)
+        self.label_14.setObjectName(u"label_14")
+        self.label_14.setGeometry(QRect(500, 360, 71, 20))
+        self.label_18 = QLabel(editLibro)
+        self.label_18.setObjectName(u"label_18")
+        self.label_18.setGeometry(QRect(20, 460, 71, 20))
+        self.lineEdit_7 = QLineEdit(editLibro)
+        self.lineEdit_7.setObjectName(u"lineEdit_7")
+        self.lineEdit_7.setGeometry(QRect(160, 460, 551, 51))
+        self.addLibro = QPushButton(editLibro)
+        self.addLibro.setObjectName(u"addLibro")
+        self.addLibro.setGeometry(QRect(110, 550, 84, 25))
+        self.addLibro.setStyleSheet(u"QPushButton\n"
+"{	\n"
+"	height: 2em;\n"
+" 	border-style: solid;\n"
+"	border-width: 2px;\n"
+" 	border-color: #0069c0;\n"
+"	font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover\n"
+"{\n"
+"   	background-color:#0069c0;\n"
+"	color:white;\n"
+"}")
+        icon = QIcon()
+        icon.addFile(u"../assets/iconos/edit-book.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.addLibro.setIcon(icon)
+        self.addLibro.setFlat(True)
+        self.addLibro_2 = QPushButton(editLibro)
+        self.addLibro_2.setObjectName(u"addLibro_2")
+        self.addLibro_2.setGeometry(QRect(240, 550, 84, 25))
+        self.addLibro_2.setStyleSheet(u"/* Style para el boton cancelar */\n"
+"QPushButton\n"
+"{	\n"
+"	height: 2em;\n"
+" 	border-style: solid;\n"
+"	border-width: 2px;\n"
+" 	border-color: grey;\n"
+"	font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover\n"
+"{\n"
+"   	background-color:grey;\n"
+"	color:white;\n"
+"}")
+        self.addLibro_2.setFlat(True)
+
+        self.retranslateUi(editLibro)
+
+        QMetaObject.connectSlotsByName(editLibro)
+    # setupUi
+
+    def retranslateUi(self, editLibro):
+        editLibro.setWindowTitle(QCoreApplication.translate("editLibro", u"Form", None))
+        self.label.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p align=\"center\"><span style=\" font-size:11pt; font-weight:600;\">EDITAR LIBRO</span></p></body></html>", None))
+        self.label_2.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Registro</span></p></body></html>", None))
+        self.label_3.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Signatura</span></p></body></html>", None))
+        self.label_4.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Fecha de Carga</span></p></body></html>", None))
+        self.label_5.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Tema</span></p></body></html>", None))
+        self.label_6.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Idioma</span></p></body></html>", None))
+        self.label_7.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Soporte</span></p></body></html>", None))
+        self.label_8.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Titulo</span></p></body></html>", None))
+        self.label_9.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p align=\"center\"><span style=\" font-size:11pt; font-weight:600;\">DATOS DEL LIBRO</span></p></body></html>", None))
+        self.label_10.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Autor Principal</span></p></body></html>", None))
+        self.label_11.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Autor Secundario</span></p></body></html>", None))
+        self.label_12.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Editorial</span></p></body></html>", None))
+        self.label_15.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">ISBN</span></p></body></html>", None))
+        self.label_16.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Lugar de Impresion</span></p><p><br/></p></body></html>", None))
+        self.label_13.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Coleccion</span></p></body></html>", None))
+        self.label_17.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">A\u00f1o de impresion</span></p></body></html>", None))
+        self.label_14.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">N\u00b0 Edicion</span></p></body></html>", None))
+        self.label_18.setText(QCoreApplication.translate("editLibro", u"<html><head/><body><p><span style=\" font-weight:600;\">Notas</span></p></body></html>", None))
+        self.addLibro.setText(QCoreApplication.translate("editLibro", u"Editar", None))
+        self.addLibro_2.setText(QCoreApplication.translate("editLibro", u"Cancelar", None))
+    # retranslateUi
+
---
+++ views/Ui_ventana_libros.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ventana_libros.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class formulario_libros(object):
+    def setupUi(self, formulario_libros):
+        if not formulario_libros.objectName():
+            formulario_libros.setObjectName(u"formulario_libros")
+        formulario_libros.resize(800, 600)
+        self.frame = QFrame(formulario_libros)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(30, 20, 721, 81))
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.botonAddLibro = QPushButton(self.frame)
+        self.botonAddLibro.setObjectName(u"botonAddLibro")
+        self.botonAddLibro.setGeometry(QRect(30, 10, 81, 51))
+        self.botonAddLibro.setStyleSheet(u"\n"
+"QPushButton:hover\n"
+"{\n"
+"	border-style: solid;\n"
+"   	background-color:#bbdefb;\n"
+"\n"
+"}\n"
+"")
+        icon = QIcon()
+        icon.addFile(u"../assets/iconos/add-book-icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.botonAddLibro.setIcon(icon)
+        self.botonAddLibro.setIconSize(QSize(45, 45))
+        self.botonAddLibro.setFlat(True)
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(30, 60, 81, 16))
+        self.botonAddLibro_2 = QPushButton(self.frame)
+        self.botonAddLibro_2.setObjectName(u"botonAddLibro_2")
+        self.botonAddLibro_2.setGeometry(QRect(120, 10, 81, 51))
+        self.botonAddLibro_2.setStyleSheet(u"\n"
+"QPushButton:hover\n"
+"{\n"
+"	border-style: solid;\n"
+"   	background-color:#bbdefb;\n"
+"\n"
+"}\n"
+"")
+        icon1 = QIcon()
+        icon1.addFile(u"../assets/iconos/edit-book.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.botonAddLibro_2.setIcon(icon1)
+        self.botonAddLibro_2.setIconSize(QSize(45, 45))
+        self.botonAddLibro_2.setFlat(True)
+        self.label_2 = QLabel(self.frame)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(120, 60, 81, 16))
+        self.frame_2 = QFrame(formulario_libros)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setGeometry(QRect(30, 110, 721, 81))
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.label_3 = QLabel(self.frame_2)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(10, 30, 81, 16))
+        self.buscarPorComboBox = QComboBox(self.frame_2)
+        self.buscarPorComboBox.setObjectName(u"buscarPorComboBox")
+        self.buscarPorComboBox.setGeometry(QRect(100, 30, 131, 22))
+        self.lineEdit = QLineEdit(self.frame_2)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(250, 30, 331, 20))
+        self.pushButton = QPushButton(self.frame_2)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(600, 20, 91, 25))
+        icon2 = QIcon()
+        icon2.addFile(u"../assets/iconos/search-icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon2)
+        self.pushButton_2 = QPushButton(self.frame_2)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(600, 50, 91, 25))
+        icon3 = QIcon()
+        icon3.addFile(u"../assets/iconos/refresh-icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_2.setIcon(icon3)
+        self.tableView = QTableView(formulario_libros)
+        self.tableView.setObjectName(u"tableView")
+        self.tableView.setGeometry(QRect(70, 210, 681, 351))
+        self.label_4 = QLabel(formulario_libros)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(90, 570, 111, 16))
+        self.cantidadLibros = QLabel(formulario_libros)
+        self.cantidadLibros.setObjectName(u"cantidadLibros")
+        self.cantidadLibros.setGeometry(QRect(210, 570, 51, 16))
+
+        self.retranslateUi(formulario_libros)
+
+        QMetaObject.connectSlotsByName(formulario_libros)
+    # setupUi
+
+    def retranslateUi(self, formulario_libros):
+        formulario_libros.setWindowTitle(QCoreApplication.translate("formulario_libros", u"lista_libros", None))
+        self.botonAddLibro.setText("")
+        self.label.setText(QCoreApplication.translate("formulario_libros", u"<html><head/><body><p align=\"center\"><span style=\" font-weight:600;\">a\u00f1adir libro</span></p></body></html>", None))
+        self.botonAddLibro_2.setText("")
+        self.label_2.setText(QCoreApplication.translate("formulario_libros", u"<html><head/><body><p align=\"center\"><span style=\" font-weight:600;\">editar libro</span></p></body></html>", None))
+        self.label_3.setText(QCoreApplication.translate("formulario_libros", u"<html><head/><body><p align=\"center\"><span style=\" font-weight:600;\">buscar por:</span></p></body></html>", None))
+        self.pushButton.setText(QCoreApplication.translate("formulario_libros", u"buscar", None))
+        self.pushButton_2.setText(QCoreApplication.translate("formulario_libros", u"actualizar", None))
+        self.label_4.setText(QCoreApplication.translate("formulario_libros", u"<html><head/><body><p><span style=\" font-weight:600;\">cantidad de libros</span></p></body></html>", None))
+        self.cantidadLibros.setText(QCoreApplication.translate("formulario_libros", u"<html><head/><body><p><br/></p></body></html>", None))
+    # retranslateUi
+
